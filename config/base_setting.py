a7359feec3966956f9bf2668de2ab8c0fef11e6c
TinsFox <1414849373@qq.com>
2019-03-18 02:41:53 +0800

    数据库无法链接

--- config/base_setting.py
+++ config/base_setting.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@www.tinsfox.com/tinsfox'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@www.tinsfox.com:3306/tinsfox'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
